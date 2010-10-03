a77e797e835a757547149d20c62ca01b86141b9a
Daniel Brockman <daniel@gointeractive.se>
2010-10-03 21:53:57 +0200

    Refactor to use #tap.

--- lib/blossom.rb
+++ lib/blossom.rb
@@ -6,47 +6,46 @@ require "compass"
 
 def Blossom(config_file, index = :index)
   root = File.dirname(config_file)
-  app = Class.new(Sinatra::Base)
-  app.class_eval do
-    configure do
-      set :root, root
-      set :public, "#{root}/static"
-      set :views, root
-      set :index, index
+  Class.new(Sinatra::Base).tap do |app|
+    app.class_eval do
+      configure do
+        set :root, root
+        set :public, "#{root}/static"
+        set :views, root
+        set :index, index
+    
+        Compass.configuration.project_path = root
+        Compass.configuration.sass_dir = ""
+        Compass.configuration.images_dir = "static"
+        Compass.configuration.http_images_path = "/"
+        Compass.configuration.output_style = :compact
+        Compass.configuration.line_comments = false
+    
+        set :haml, { :format => :html5, :attr_wrapper => '"' }
+        set :sass, Compass.sass_engine_options
+      end
+    
+      get "/" do
+        haml settings.index
+      end
   
-      Compass.configuration.project_path = root
-      Compass.configuration.sass_dir = ""
-      Compass.configuration.images_dir = "static"
-      Compass.configuration.http_images_path = "/"
-      Compass.configuration.output_style = :compact
-      Compass.configuration.line_comments = false
+      def self.file_exists? suffix
+        condition do
+          basename = File.basename(request.path)
+          barename = basename.sub(/\.[^.]*$/, '')
+          name = "#{barename}.#{suffix}"
+          File.exist? File.join(settings.root, name)
+        end
+      end
+    
+      get "/:name.css", :file_exists? => :sass do
+        content_type "text/css", :charset => "utf-8"
+        sass params[:name].to_sym
+      end
   
-      set :haml, { :format => :html5, :attr_wrapper => '"' }
-      set :sass, Compass.sass_engine_options
-    end
-  
-    get "/" do
-      haml settings.index
-    end
-
-    def self.file_exists? suffix
-      condition do
-        basename = File.basename(request.path)
-        barename = basename.sub(/\.[^.]*$/, '')
-        name = "#{barename}.#{suffix}"
-        File.exist? File.join(settings.root, name)
+      get "/:name", :file_exists? => :haml do
+        haml params[:name].to_sym
       end
     end
-  
-    get "/:name.css", :file_exists? => :sass do
-      content_type "text/css", :charset => "utf-8"
-      sass params[:name].to_sym
-    end
-
-    get "/:name", :file_exists? => :haml do
-      haml params[:name].to_sym
-    end
   end
-
-  app
 end
